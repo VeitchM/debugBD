3f422475439ae02b47fa1238f59e8233e59a8de1
unknown <matiasveitch@gmail.com>
2022-11-27 14:49:26 -0300

    Nice server

--- programilla/index.js
+++ programilla/index.js
@@ -1,20 +1,57 @@
 require('dotenv').config();
 
+
+let lastCleaning = "No se conoce"
+
+// PostgreSQL 
+
 const { Client } = require('pg');
 const client = new Client();
-client.connect();
+let DBConnected=false;
 
-const express = require('express')
-const app = express()
+function connectDB(){
+    client.connect((err) => {
+        if (err){
+            console.log('Connection failed');
+            DBConnected = false;
+        }
+        else {
+            console.log('Connected to DB');
+            DBConnected = true;
+        }
+    });
+}
+
+async function deleteDoubles() {
+    throw new Error('Ouch')
+    //return await client.query("delete from users u where u.id in (select distinct u.id from public.users u, public.users v where u.uid=v.uid and u.id <>v.id and u.dni isnull)");
+}
+
+
+// Express Code
+const app = require('express')();  
+
+app.set('view engine','ejs');
 
 app.get('/', (req, res) => {
-    borrarDuplicados().then((x)=>{res.send("La cantidad de Usuarios Duplicados es: " + x.rowCount)})
+    deleteDoubles()
+    .then( (x)=>{
+        lastCleaning = new Date();
+        res.render('pages/index',{DBAnswer : x, lastCleaning : lastCleaning})
+        console.log("Then");
+    }
+    ,(e) => {
+        console.log('Failed',e);
+        res.render('pages/index',{DBAnswer : e, lastCleaning : lastCleaning});
+    })
+
+
     //res.send('hello world')
 })
 
-app.listen(process.env.PORT)
-async function borrarDuplicados() {
-    return await client.query("delete from users u where u.id in (select distinct u.id from public.users u, public.users v where u.uid=v.uid and u.id <>v.id and u.dni isnull)");
-}
 
-borrarDuplicados().then((x) => { console.log(x.rowCount); });
+//---------------------------------------------------------
+
+connectDB();
+app.listen(process.env.PORT)
+
