c52f90fad327f9dcf20edc8a6adac82c724e3e46
unknown <matiasveitch@gmail.com>
2022-11-27 16:54:18 -0300

    Made too many static pages

--- programilla/index.js
+++ programilla/index.js
@@ -7,43 +7,45 @@ let lastCleaning = "No se conoce"
 
 const { Client } = require('pg');
 const client = new Client();
-let DBConnected=false;
+let DBConnected = false;
 
-function connectDB(){
-    client.connect((err) => {
-        if (err){
-            console.log('Connection failed');
-            DBConnected = false;
-        }
-        else {
-            console.log('Connected to DB');
-            DBConnected = true;
-        }
-    });
+async function connectDB() {
+    try {
+
+        await client.connect()
+        console.log('Connected to DB');
+        DBConnected = true;
+
+    }
+    catch (err) {
+        console.log('Connection failed');
+        DBConnected = false;
+        throw Error("Connection to Database Failed")
+    }
+
 }
 
 async function deleteDoubles() {
-    throw new Error('Ouch')
-    //return await client.query("delete from users u where u.id in (select distinct u.id from public.users u, public.users v where u.uid=v.uid and u.id <>v.id and u.dni isnull)");
+    //throw new Error('Ouch')
+    return await client.query("delete from users u where u.id in (select distinct u.id from public.users u, public.users v where u.uid=v.uid and u.id <>v.id and u.dni isnull)");
 }
 
 
 // Express Code
-const app = require('express')();  
+const app = require('express')();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    deleteDoubles()
-    .then( (x)=>{
-        lastCleaning = new Date();
-        res.render('pages/index',{DBAnswer : x, lastCleaning : lastCleaning})
-        console.log("Then");
+app.get('/', async (req, res) => {
+
+    if (!DBConnected) {
+        res.render('pages/connectDB', { lastCleaning: lastCleaning })
+        connectDB().then(
+            () => answer(res),
+            (e) => res.render('pages/failed', { DBAnswer: e, lastCleaning: lastCleaning })
+        );
     }
-    ,(e) => {
-        console.log('Failed',e);
-        res.render('pages/index',{DBAnswer : e, lastCleaning : lastCleaning});
-    })
+    answer(res);
 
 
     //res.send('hello world')
@@ -54,4 +56,17 @@ app.get('/', (req, res) => {
 
 connectDB();
 app.listen(process.env.PORT)
+
+function answer(res) {
+    deleteDoubles()
+        .then((x) => {
+            lastCleaning = new Date();
+            res.render('pages/index', { DBAnswer: x, lastCleaning: lastCleaning });
+            console.log("Then");
+        },
+            (e) => {
+                console.log('Failed', e);
+                res.render('pages/failed', { DBAnswer: e, lastCleaning: lastCleaning });
+            });
+}
 
